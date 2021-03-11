31394d21814f481623c1245d77ca7b72a9adc732
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-03-11 08:53:11 -0500

    installed axios and started with initial api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const questionAPIURL = 'https://opentdb.com/api.php?amount=10&category=15'
+    const apiCall = async () => {
+      const response = await axios(questionAPIURL);
+      setQuestions(response.data);
+      console.log(response.data.results)
+    }
+    apiCall();
+  }, [])
+
   return (
     <div className="App">
 
