a0611ca712a407afb000f1a3d7e54285455c2cbb
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-03-12 08:41:52 -0500

    added healthbar component, added it inside questions component

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import HealthBar from 'HealthBar'
 
 export default function Questions(props) {
   const { questions } = props;
@@ -6,9 +7,18 @@ export default function Questions(props) {
   console.log(props);
   const [currentQuestion, setCurrentQuestion] = useState(0)
 
+  const healthPoints = [
+    { bgcolor: green, completed: 100 },
+    { bgcolor: yellow, completed: 66.3 },
+    { bgcolor: red, completed: 33.0}
+  ]
+
   return (
     <>
       <div className="question-container">
+        <div className='health-container'>
+          <HealthBar healthPoints={healthPoints}/>
+        </div>
         <div className='question'>
           <p>{questions.results[currentQuestion].question}</p>
         </div>
