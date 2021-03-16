03387f4f4a5324d51e4e78a631b9a80c4bc9884d
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2021-03-16 14:12:39 -0400

    added answer choices

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -1,30 +1,45 @@
 import React, { useState } from "react";
+import GameOver from "../../Screens/GameOver/GameOver";
 
 export default function Questions(props) {
   const { questions } = props;
-  console.log(questions.results.length)
-  console.log(questions.results[0].question)
-  console.log(questions.results[0].incorrect_answers)
+  console.log(questions.results.length);
+  console.log(questions.results[0].question);
+  console.log(questions.results[0].incorrect_answers);
   console.log(props);
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [totalWrong, setTotalWrong] = useState(0);
+
+  if (totalWrong === 3) {
+    return <GameOver />;
+  }
+
+  let correctAnswer = new Array(
+    questions.results[currentQuestion].correct_answer
+  );
+  let inCorrectAnswers = questions.results[currentQuestion].incorrect_answers;
+  let allAnswers = correctAnswer.concat(inCorrectAnswers).sort();
+  console.log(allAnswers);
+  console.log(allAnswers.length)
 
   return (
     <>
       <div className="question-container">
-
-        <div className='question'>
-          <p>{questions.results[currentQuestion].question}</p>
-        </div>
-        <div className='total-questions'>
+        <div className="total-questions">
           <span>Question {currentQuestion + 1}</span>/{questions.results.length}
         </div>
+
+        <div className="question">
+          <p>{questions.results[currentQuestion].question}</p>
+        </div>
+
         <div className='answers-container'>
-          <div className='answers'>
-            <p>{questions.results[currentQuestion].correct_answer}</p>
-            <div className='incorrect'>
-              {questions.results[currentQuestion].incorrect_answers.map((incorrect) => <p>{incorrect.incorrect_answers}</p>)}
-            </div>
-          </div>
+          {allAnswers.map((answer) => {
+            return (
+              <button>{answer}</button>
+            )
+          })}
         </div>
       </div>
     </>
